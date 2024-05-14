c7bfd4b7a22f1c70ce698cd40ae3e85b8e5ba0c3
banicaDaniel07 <banica.daniel07@gmail.com>
2024-05-14 23:29:38 +0300

    Added all the style in tailwind

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -26,27 +26,18 @@ const Projects = () => {
         Your collaboration is highly valued!
       </p>
 
-      <div style={{
-        position: 'relative'
-      }}>
-        <div className="container grid-cols-2fr-1fr-2fr" style={{
-          borderRadius: 12,
-          gridGap: '1.5rem',
-          padding: '3rem',
-          background: 'linear-gradient(135deg,#b1f,#a0f)',
-        }}>
+      <div className="relative"
+      >
+        <div
+          className="grid-cols-2fr-1fr-2fr m-0 h-full w-full grid relative gap-6 px-12 py-16 rounded-xl my-10"
+          style={{
+            background: 'linear-gradient(135deg,#b1f,#a0f)',
+          }}
+        >
           {projects.map((item) => (
             <motion.div
-              className={' kid' + item.id}
+              className={`project-${item.id} h-48 will-change-transform bg-white flex flex-col rounded-xl items-center justify-center`}
               style={{
-                height: 200,
-                willChange: 'transform',
-                background: '#fff',
-                display: 'flex',
-                flexDirection: 'column',
-                borderRadius: 12,
-                alignItems: 'center',
-                justifyContent: 'center',
                 boxShadow: '0 2px 3px 0 rgba(0, 0, 0, .1), 0 10px min(calc(1rem*(15 /(18 + .167* 2)))), 15px) 0 rgba(0, 0, 0, .06)'
               }}
               key={item.id}
@@ -65,43 +56,22 @@ const Projects = () => {
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 0.8 }}
                 exit={{ opacity: 0, transition: { duration: 0.2 } }}
+                className='absolute w-full h-full top-0 left-0 right-0 bottom-0 rounded-xl'
                 style={{
-                  position: 'absolute',
-                  width: '100%',
-                  height: '100%',
-                  top: 0,
-                  left: 0,
-                  right: 0,
-                  bottom: 0,
                   background: 'linear-gradient(135deg,#b1f,#a0f)'
                 }}
-              >
-              </motion.div>
-              <div style={{
-                position: 'absolute',
-                display: 'flex',
-                alignItems: 'center',
-                width: '100%',
-                height: '100%',
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                justifyContent: 'center',
-                background: 'transparent',
-                boxShadow: '0 2px 3px 0 rgba(0, 0, 0, .1), 0 10px min(calc(1rem* 15), 15px) 0 rgba(0, 0, 0, .06)'
-              }}>
+              />
+              <div
+                className='absolute flex items-center justify-center w-full h-full top-0 left-0 right-0 bottom-0 bg-transparent'
+                style={{
+                  boxShadow: '0 2px 3px 0 rgba(0, 0, 0, .1), 0 10px min(calc(1rem* 15), 15px) 0 rgba(0, 0, 0, .06)'
+                }}>
                 <motion.div
                   layoutId={selectedId}
+                  className='bg-white rounded-xl flex flex-col justify-center items-center cursor-pointer'
                   style={{
-                    backgroundColor: '#fff',
                     width: 650,
                     height: 350,
-                    borderRadius: 12,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center'
                   }}
                 >
                   <motion.h5>{projects.find((item) => item.id === selectedId)?.subtitle}</motion.h5>
