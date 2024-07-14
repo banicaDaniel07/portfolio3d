b6eb19431ef12b6af7156a5de8721814c4c87ae3
banicaDaniel07 <banica.daniel07@gmail.com>
2024-07-14 22:00:05 +0300

    Added mobile view for CTA

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -4,7 +4,7 @@ const CTA = () => {
   return (
     <section className='cta'>
       <p className='cta-text'>
-        Have a project in mind? <br className='sm:block hidden' />
+        Have a project in mind? <br />
         Let’s build something together!
       </p>
       <Link to='/contact' className='btn text-white !w-24 hover:scale-105'>
